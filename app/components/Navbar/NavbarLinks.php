15b67d87c08d06889bc20844d2de3f789d91fe32
Lukas Velek <lukas.velek@centrum.cz>
2024-07-08 15:51:11 +0200

    Topic invites navbar link

--- app/components/Navbar/NavbarLinks.php
+++ app/components/Navbar/NavbarLinks.php
@@ -9,6 +9,7 @@ class NavbarLinks {
     public const USER_PROFILE = ['page' => 'UserModule:Users', 'action' => 'profile'];
     public const USER_LOGOUT = ['page' => 'UserModule:Logout', 'action' => 'logout'];
     public const ADMINISTRATION = ['page' => 'AdminModule:Home', 'action' => 'dashboard'];
+    public const USER_INVITES = ['page' => 'UserModule:TopicInvites', 'action' => 'list'];
 
     public static function toArray() {
         return [
