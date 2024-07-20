21130c23672dec0023142dbcd7474451c786f232
Lukas Velek <lukas.velek@centrum.cz>
2024-07-20 19:26:19 +0200

    LinkBuilder code comments

--- app/ui/LinkBuilder.php
+++ app/ui/LinkBuilder.php
@@ -2,17 +2,30 @@
 
 namespace App\UI;
 
+/**
+ * Link Builder allows to easily make links.
+ * 
+ * @author Lukas Velek
+ */
 class LinkBuilder implements IRenderable {
     private array $elements;
     private string $text;
     private array $urlParts;
 
+    /**
+     * Class constructor
+     */
     public function __construct() {
         $this->elements = [];
         $this->text = '';
         $this->urlParts = [];
     }
 
+    /**
+     * Renders the class parameters to HTML code
+     * 
+     * @return string HTML code
+     */
     public function render() {
         if(!empty($this->urlParts)) {
             $this->processUrl();
@@ -23,40 +36,81 @@ class LinkBuilder implements IRenderable {
         return $code;
     }
 
+    /**
+     * Sets the link class attribute
+     * 
+     * @param string $class Class name
+     * @return self
+     */
     public function setClass(string $class) {
         $this->elements['class'] = $class;
 
         return $this;
     }
 
+    /**
+     * Sets the link href attribute
+     * 
+     * @param string $href Href
+     * @return self
+     */
     public function setHref(string $href) {
         $this->elements['href'] = $href;
+
+        return $this;
     }
 
+    /**
+     * Sets the link href attribute from URL
+     * 
+     * @param array $url URL array
+     * @return self
+     */
     public function setUrl(array $url) {
         $this->urlParts = array_merge($this->urlParts, $url);
 
         return $this;
     }
 
+    /**
+     * Sets the link text
+     * 
+     * @param string $text Text
+     * @return self
+     */
     public function setText(string $text) {
         $this->text = $text;
 
         return $this;
     }
 
+    /**
+     * Sets the link style attribute
+     * 
+     * @param string $style Style
+     * @return self
+     */
     public function setStyle(string $style) {
         $this->elements['style'] = $style;
 
         return $this;
     }
 
+    /**
+     * Sets the link onclick attribute
+     * 
+     * @param string JS onclick method name
+     * @return self
+     */
     public function setOnclick(string $onclickMethod) {
         $this->elements['onclick'] = $onclickMethod;
 
         return $this;
     }
 
+    /**
+     * Processes the URL array into a string URL and finally saves it to the href attribute
+     */
     private function processUrl() {
         $tmp = [];
 
@@ -64,9 +118,14 @@ class LinkBuilder implements IRenderable {
             $tmp[] = $k . '=' . $v;
         }
 
-        $this->elements['href'] = '?' . implode('&', $tmp);
+        $this->setHref('?' . implode('&', $tmp));
     }
 
+    /**
+     * Processes all the link attributes
+     * 
+     * @return string Link attributes
+     */
     private function processElements() {
         $tmp = [];
         $tmpSingles = [];
@@ -84,12 +143,28 @@ class LinkBuilder implements IRenderable {
         return implode(' ', $tmp);
     }
 
+    /**
+     * Creates a simple link
+     * 
+     * @param string $text Link text
+     * @param array $URL Link URL
+     * @param string $class Link class
+     * @return string HTML code
+     */
     public static function createSimpleLink(string $text, array $url, string $class) {
         $obj = self::createSimpleLinkObject($text, $url, $class);
 
         return $obj->render();
     }
 
+    /**
+     * Creates a simple link and returns self
+     * 
+     * @param string $text Link text
+     * @param array $URL Link URL
+     * @param string $class Link class
+     * @return self
+     */
     public static function createSimpleLinkObject(string $text, array $url, string $class) {
         $lb = new self();
 
@@ -100,6 +175,12 @@ class LinkBuilder implements IRenderable {
         return $lb;
     }
 
+    /**
+     * Converts URL array to string
+     * 
+     * @param array $url
+     * @return string URL
+     */
     public static function convertUrlArrayToString(array $url) {
         $tmp = [];
         foreach($url as $k => $v) {
@@ -109,6 +190,14 @@ class LinkBuilder implements IRenderable {
         return '?' . implode('&', $tmp);
     }
 
+    /**
+     * Creates a JS link with onclick attribute
+     * 
+     * @param string $text Link text
+     * @param string $jsMethod JS method called on onclick action
+     * @param string $class Link class
+     * @return string HTML code
+     */
     public static function createJSOnclickLink(string $text, string $jsMethod, string $class) {
         $lb = new self();
 
