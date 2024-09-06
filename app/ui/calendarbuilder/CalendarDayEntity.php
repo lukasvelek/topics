fca00f87043e78ab6c27d54d7d00abbd6c2e94c2
Lukas Velek <lukas.velek@centrum.cz>
2024-09-06 09:34:27 +0200

    Code comments

--- app/ui/calendarbuilder/CalendarDayEntity.php
+++ app/ui/calendarbuilder/CalendarDayEntity.php
@@ -2,19 +2,40 @@
 
 namespace App\UI\CalendarBuilder;
 
+/**
+ * CalendarDayEntity represents a day in calendar
+ * 
+ * @author Lukas Velek
+ */
 class CalendarDayEntity {
     private string $header;
     private array $events;
 
+    /**
+     * Class constructor
+     * 
+     * @param string $header Header (day number or empty string)
+     * @param array $events Array of events for the given day
+     */
     public function __construct(string $header, array $events = []) {
         $this->header = $header;
         $this->events = $events;
     }
 
+    /**
+     * Returns if the day is empty
+     * 
+     * @return bool True if the day is empty or false if not
+     */
     public function isEmpty() {
         return $this->header == '';
     }
 
+    /**
+     * Renders the day content
+     * 
+     * @return string HTML code
+     */
     public function render() {
         $code = '<b>' . $this->header . '</b>';
 
--- app/ui/calendarbuilder/CalendarWeekEntity.php
+++ app/ui/calendarbuilder/CalendarWeekEntity.php
@@ -2,17 +2,28 @@
 
 namespace App\UI\CalendarBuilder;
 
+/**
+ * CalendarWeekEntity represents a week in calendar
+ * 
+ * @author Lukas Velek
+ */
 class CalendarWeekEntity {
     private array $days;
 
+    /**
+     * Class constructor
+     * 
+     * @param array $days Array of instances of CalendarDayEntity that represent single days
+     */
     public function __construct(array $days) {
         $this->days = $days;
     }
 
-    public function getDay(int $index) {
-        return $this->days[$index];
-    }
-
+    /**
+     * Returns all days
+     * 
+     * @return array Days
+     */
     public function getDays() {
         return $this->days;
     }
