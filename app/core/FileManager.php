bd4fbcb0379e6599b516778fefd9e3d7ec4decd3
Lukas Velek <lukas.velek@centrum.cz>
2024-09-16 14:57:36 +0200

    Code comments

--- app/core/FileManager.php
+++ app/core/FileManager.php
@@ -2,16 +2,31 @@
 
 namespace App\Core;
 
-use App\Exceptions\AException;
 use App\Exceptions\FileDoesNotExistException;
-use App\Exceptions\FolderDeleteException;
-use Exception;
 
+/**
+ * FileManager allows manipulating with files.
+ * 
+ * @author Lukas Velek
+ */
 class FileManager {
+    /**
+     * Checks if file exists
+     * 
+     * @param string $filePath Path to the file
+     * @return bool True if file exists or false if not
+     */
     public static function fileExists(string $filePath) {
         return file_exists($filePath);
     }
 
+    /**
+     * Loads file content
+     * 
+     * @param string $filePath Path to the file
+     * @return string|false File content or false if error occurred
+     * @throws FileDoesNotExistException
+     */
     public static function loadFile(string $filePath) {
         if(!self::fileExists($filePath)) {
             throw new FileDoesNotExistException($filePath);
@@ -20,10 +35,22 @@ class FileManager {
         return file_get_contents($filePath);
     }
 
+    /**
+     * Checks if folder exists
+     * 
+     * @param string $dirPath Path to the directory
+     * @return bool True if directory exists or false if not
+     */
     public static function folderExists(string $dirPath) {
         return is_dir($dirPath);
     }
 
+    /**
+     * Returns all files in a given directory
+     * 
+     * @param string $dirPath Path to the directory (root in method's context)
+     * @return array Array of filenames (key is relative path and value is absolute path)
+     */
     public static function getFilesInFolder(string $dirPath) {
         $recursive = function (string $dirPath, array &$objects) {
             $contents = scandir($dirPath);
@@ -48,6 +75,16 @@ class FileManager {
         return $objects;
     }
 
+    /**
+     * Saves file
+     * 
+     * @param string $path Path to the directory where the file will be saved
+     * @param string $filename Filename
+     * @param string|array $fileContent File content
+     * @param bool $overwrite True if file content should be overwritten or false if not
+     * @param bool $appendNewLine Only applicable if $fileContent is array. True if each element in $fileContent should be on a new line, or false if not
+     * @return int|false Number of bytes written or false if error occurred
+     */
     public static function saveFile(string $path, string $filename, string|array $fileContent, bool $overwrite = false, bool $appendNewLine = true) {
         if(is_array($fileContent)) {
             if($appendNewLine) {
@@ -68,10 +105,23 @@ class FileManager {
         }
     }
 
+    /**
+     * Creates a directory
+     * 
+     * @param string $dirPath Path to the directory
+     * @param bool $recursive True if directory should be created recursively or false if not
+     * @return bool True on success or false on failure
+     */
     public static function createFolder(string $dirPath, bool $recursive = false) {
         return mkdir($dirPath, 0777, $recursive);
     }
 
+    /**
+     * Deletes a folder recursively
+     * 
+     * @param string $dirPath Path to the directory that should be deleted
+     * @return bool True on success or false on failure
+     */
     public static function deleteFolderRecursively(string $dirPath) {
         if(is_dir($dirPath)) {
             $objects = scandir($dirPath);
@@ -86,8 +136,10 @@ class FileManager {
                 }
             }
 
-            rmdir($dirPath);
+            return rmdir($dirPath);
         }
+
+        return false;
     }
 }
 
