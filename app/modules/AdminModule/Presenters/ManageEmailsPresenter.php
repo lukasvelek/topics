e1fe5e02848ae2d5c4900ab2ab3ebfa982523586
Lukas Velek <lukas.velek@centrum.cz>
2024-09-26 20:56:29 +0200

    Presenter creation

---
+++ app/modules/AdminModule/Presenters/ManageEmailsPresenter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\AdminModule;
+
+class ManageEmailsPresenter extends AAdminPresenter {
+    public function __construct() {
+        parent::__construct('ManageEmailsPresenter', 'Email management');
+    }
+}
+
+?>
