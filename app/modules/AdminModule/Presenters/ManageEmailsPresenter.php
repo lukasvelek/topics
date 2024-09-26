f31cf51efcef16e07696c460ff1a12940d9ee6b2
Lukas Velek <lukas.velek@centrum.cz>
2024-09-26 21:05:17 +0200

    Implementation

--- app/modules/AdminModule/Presenters/ManageEmailsPresenter.php
+++ app/modules/AdminModule/Presenters/ManageEmailsPresenter.php
@@ -2,9 +2,19 @@
 
 namespace App\Modules\AdminModule;
 
+use App\Helpers\GridHelper;
+
 class ManageEmailsPresenter extends AAdminPresenter {
+    private GridHelper $gridHelper;
+
     public function __construct() {
         parent::__construct('ManageEmailsPresenter', 'Email management');
+    }
+
+    public function startup() {
+        parent::startup();
+
+        $this->gridHelper = new GridHelper($this->logger, $this->getUserId());
     }
 }
 
